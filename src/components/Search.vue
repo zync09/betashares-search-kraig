646681edab522684a3fd02ccec3fb10317de20e9
Kraig <kraig@frankbody.com>
2025-04-21 10:39:22 +1000

    Refactor App.vue to replace HelloWorld component with Search component, implement search functionality with new composables, and update Vite configuration to set server port and host. Add ClearButton, LoadingSpinner, and SearchIcon components.

---
+++ src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="flex items-center gap-2 w-full">
+    <div class="py-5 px-4 rounded-full bg-white w-full flex items-center gap-2 space-between">
+      <input type="text" v-model="searchText" class="w-full focus:outline-none pl-3" placeholder="Search the site by ASX code, fund name, phrase or topic" />
+      <LoadingSpinner v-show="isLoading" />
+      <ClearButton v-show="searchText.length > 0" @click.prevent="clearResults" />
+    </div>
+    <button type="submit" class="p-2 rounded-full bg-primary text-white hover:text-black transition-all duration-300">
+      <SearchIcon />
+    </button>
+  </form>
+  <!-- results -->
+  <div class="flex flex-col gap-2" data-id="results" v-if="searchResultsData?.results?.length > 0">
+    <pre>{{ searchResultsData }}</pre>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { watchEffect } from 'vue'
+import { useSearch } from '../composables/Search'
+
+import ClearButton from './ui/ClearButton.vue'
+import SearchIcon from '../svg/SearchIcon.vue'
+import LoadingSpinner from './ui/LoadingSpinner.vue'
+
+const { searchResultsData, isLoading, startSearch, clearResults, searchText } = useSearch()
+
+watchEffect(async () => {
+  if (searchText.value.length > 0) {
+    await startSearch(searchText.value)
+  } else {
+    clearResults()
+  }
+})
+
+</script>
---
+++ src/components/ui/LoadingSpinner.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="w-5 h-5 border-2 border-t-primary border-b-slate-300 border-r-slate-300 border-l-slate-300 rounded-full animate-spin"></div>
+</template>
